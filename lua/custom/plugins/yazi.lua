0019c74d0e75ab30b63126bbf91b4faefee91187
Alex Wendland <a.p.wendland@gmail.com>
2025-05-23 21:13:12 +0100

    Modify yazi to minimal

--- lua/custom/plugins/yazi.lua
+++ lua/custom/plugins/yazi.lua
@@ -4,23 +4,4 @@ return {
   dependencies = {
     'folke/snacks.nvim',
   },
-  keys = {
-    {
-      '<leader>tt',
-      '<cmd>Yazi<cr>',
-      mode = { 'n', 'v' },
-      desc = '[T]oggle [T]ree',
-    },
-    {
-      '<leader>tr',
-      '<cmd>Yazi cwd<cr>',
-      desc = '[T]oggle [R]oot',
-    },
-  },
-  opts = {
-    open_for_directories = true,
-    keymaps = {
-      show_help = '<f1>',
-    },
-  },
 }
