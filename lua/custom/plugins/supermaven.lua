d7a838dfa0238de37f85bcce604816716d5a806c
Alex Wendland <a.p.wendland@gmail.com>
2025-05-24 14:45:49 +0100

    Turn off suggestions without cmp

--- lua/custom/plugins/supermaven.lua
+++ lua/custom/plugins/supermaven.lua
@@ -3,4 +3,8 @@ return {
   config = function()
     require('supermaven-nvim').setup {}
   end,
+  opts = {
+    disable_inline_completion = true, -- disables inline completion for use with cmp
+    disable_keymaps = true, -- disables built in keymaps for more manual control
+  },
 }
