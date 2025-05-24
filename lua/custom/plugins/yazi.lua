d4230f898797fd34d0b1f87033b9afc39b11152a
Alex Wendland <a.p.wendland@gmail.com>
2025-05-24 10:10:51 +0100

    Add keys back to yazi

--- lua/custom/plugins/yazi.lua
+++ lua/custom/plugins/yazi.lua
@@ -4,4 +4,23 @@ return {
   dependencies = {
     'folke/snacks.nvim',
   },
+  keys = {
+    {
+      '<leader>tt',
+      '<cmd>Yazi<cr>',
+      mode = { 'n', 'v' },
+      desc = '[T]oggle [T]ree',
+    },
+    {
+      '<leader>tr',
+      '<cmd>Yazi cwd<cr>',
+      desc = '[T]oggle [R]oot',
+    },
+  },
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = '<f1>',
+    },
+  },
 }
