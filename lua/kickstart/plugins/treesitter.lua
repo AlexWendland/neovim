2b9c14963a03921303355b4cb8c880d2166d9c05
Alex Wendland <a.p.wendland@gmail.com>
2026-04-13 16:11:31 +0100

    feat: Try to fix yazi problem

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -9,11 +9,14 @@ return {
       require('nvim-treesitter').install { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'cpp', 'python', 'nix', 'typescript', 'rust', 'go' }
 
       -- Enable treesitter highlighting per filetype, auto-installing parsers on demand
+      local ts = require 'nvim-treesitter'
+      local parsers = require 'nvim-treesitter.parsers'
+
       vim.api.nvim_create_autocmd('FileType', {
         callback = function()
           local ft = vim.bo.filetype
-          if ft and ft ~= '' then
-            require('nvim-treesitter').install { ft }
+          if ft and ft ~= '' and parsers[ft] then
+            ts.install { ft }
           end
           pcall(vim.treesitter.start)
         end,
